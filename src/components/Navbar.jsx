9b61686760e56e734b3f75beb6f4192bf004ec4f
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-28 05:11:16 +0100

    added everything needed for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import NotFound from './pages/NotFound.jsx'
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </>
+  )
 }
 
 export default Navbar
