e0537dec427df38e5a3762ef60379a57ecd43691
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-28 05:32:58 +0100

    added styling to the project

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const NotFound = () => {
-  return <div>NotFound</div>
+  return (
+    <div className="flex flex-col text-center items-center justify-center h-[100vh] w-[100vw]">
+      NotFound
+    </div>
+  )
 }
 
 export default NotFound
